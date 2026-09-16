55a04d1a222750e7136c9a940237d6fbb565d3dc
meaningstone <164450325+meaningstone@users.noreply.github.com>
2026-09-16 18:49:43 +0900

    [Passed] 반복 출력하기 (2ms, 0MB)

---
+++ trail2/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+void print(int n) {
+    for(int i=0; i<n; i++) {
+        cout << "12345^&*()_" << endl;
+    }
+}
+
+int main() {
+    cin >> N;
+    print(N);
+    // Please write your code here.
+    return 0;
+}
